04ab21adf38f31f205840c4934c2acd817e64530
meepo <noonecanstop@gmail.com>
2014-05-08 15:23:24 +0800

    project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectResume::Application.config.secret_token = '4e1b7413807146176e6906f5495f3a94374f7997f602e5471f7afbb13039f4f1acd12c753bcad3b7ef772173fb7ae739496b9f5065ad6b7f1a2612340afededf'
